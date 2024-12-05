5481ea2b73d46d2861e7a8933c502e0da3b72933
Jenn Scheer <jenn@jennscheer.com>
2024-12-04 23:01:15 -0500

    Change reference highlight padding based on font size

--- src/app/helpers/portableTextComponents.tsx
+++ src/app/helpers/portableTextComponents.tsx
@@ -9,10 +9,7 @@ export const portableTextComponents = {
       value: PatternInlineReferenceBlockDto;
     }) => {
       return (
-        <Link
-          href={`/patterns/${value.slug}`}
-          className="uppercase text-base bg-accent-highlight p-0.5 rounded mx-px hover:bg-accent-highlight-hover transition-colors"
-        >
+        <Link href={`/patterns/${value.slug}`} className="reference_highlight">
           {value.name} <span className="text-stone-500">{value.number}</span>
         </Link>
       );
