f85b30c48ae3ed61851502e8c4a993ae98b1cab3
Jenn Scheer <jenn@jennscheer.com>
2024-12-08 15:52:48 -0500

    Hide pattern sidebar on patterns page

--- src/app/(site)/patterns/[slug]/layout.tsx
+++ src/app/(site)/patterns/[slug]/layout.tsx
@@ -1,21 +1,21 @@
-import PatternsSidebar from "@/app/components/PatternsSidebar";
-import { sanityFetch } from "@/sanity/lib/live";
-import { SectionDto } from "@/app/helpers/types";
-import { sectionsQuery } from "@/sanity/lib/queries";
+// import PatternsSidebar from "@/app/components/PatternsSidebar";
+// import { sanityFetch } from "@/sanity/lib/live";
+// import { SectionDto } from "@/app/helpers/types";
+// import { sectionsQuery } from "@/sanity/lib/queries";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default async function PatternLayout(props: Props) {
-  const { data: sections }: { data: SectionDto[] } = await sanityFetch({
-    query: sectionsQuery,
-  });
+  // const { data: sections }: { data: SectionDto[] } = await sanityFetch({
+  //   query: sectionsQuery,
+  // });
 
   return (
     <>
       {props.children}
-      <PatternsSidebar sections={sections} />
+      {/* <PatternsSidebar sections={sections} /> */}
     </>
   );
 }
