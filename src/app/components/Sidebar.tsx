571f2ef8587042dcfd482e83b54c1e7f3e70600f
Jenn Scheer <jenn@jennscheer.com>
2025-02-15 17:56:30 -0700

    Add cursor pointer for menu sidebar button

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -9,7 +9,8 @@ type SidebarProps = {
   renderContent: (onClick?: () => void) => React.ReactNode;
 };
 
-const tableOfContentsClasses = 'flex fixed z-10 left-0 top-0 h-10 pl-5 items-center text-sm';
+const tableOfContentsClasses =
+  'flex fixed z-10 left-0 top-0 h-10 pl-5 items-center text-sm cursor-pointer';
 const sidebarWrapperClasses = 'fixed z-10 left-0 top-10.5 w-56 bg-accent-200 overflow-hidden';
 const sidebarClasses =
   'flex flex-col gap-y-8 h-screen overflow-y-scroll w-60 pt-5 pl-5 pr-9 sm:pr-5 pb-20';
